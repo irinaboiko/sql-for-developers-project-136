3e26fc4ce69a6d63d76118e67f0634780e221546
irinaboiko <irinaboiko95@gmail.com>
2024-09-19 22:59:59 +0300

    remove comments

--- database.sql
+++ database.sql
@@ -1,6 +1,3 @@
-CREATE DATABASE pg_tables;
-
--- Таблица уроков
 CREATE TABLE lessons(
 	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	course_id BIGINT REFERENCES courses(id) NOT NULL,
@@ -13,7 +10,6 @@ CREATE TABLE lessons(
 	deleted_at TIMESTAMP NOT NULL
 );
 
--- Таблица курсов
 CREATE TABLE courses(
 	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	name VARCHAR(255) NOT NULL,
@@ -23,7 +19,6 @@ CREATE TABLE courses(
 	deleted_at TIMESTAMP NOT NULL
 );
 
--- Таблица модулей
 CREATE TABLE modules(
 	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	name VARCHAR(255) NOT NULL,
@@ -33,7 +28,6 @@ CREATE TABLE modules(
 	deleted_at TIMESTAMP NOT NULL
 );
 
--- Таблица программ обучения
 CREATE TABLE programs(
 	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	name VARCHAR(255) NOT NULL,
@@ -43,19 +37,16 @@ CREATE TABLE programs(
   updated_at TIMESTAMP NOT NULL
 );
 
--- Таблица для связи курсов и модулей (многие-ко-многим)
 CREATE TABLE course_modules(
 	course_id BIGINT REFERENCES courses(id) ON DELETE SET NULL,
 	module_id BIGINT REFERENCES modules(id) ON DELETE SET NULL
 );
 
--- Таблица для связи программ и модулей (многие-ко-многим)
 CREATE TABLE program_modules(
 	program_id BIGINT REFERENCES programs(id) ON DELETE SET NULL,
 	module_id BIGINT REFERENCES modules(id) ON DELETE SET NULL
 );
 
--- Таблица пользователей
 CREATE TYPE user_role AS ENUM('student', 'teacher', 'admin');
 
 CREATE TABLE users(
@@ -70,7 +61,6 @@ CREATE TABLE users(
   deleted_at TIMESTAMP NOT NULL
 );
 
--- Таблица учебной группы пользователя
 CREATE TABLE teaching_groups(
 	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	slug VARCHAR(255) UNIQUE,
@@ -78,7 +68,6 @@ CREATE TABLE teaching_groups(
   updated_at TIMESTAMP NOT NULL
 );
 
--- Таблица подписок
 CREATE TYPE enrollment_status AS ENUM('active', 'pending', 'cancelled', 'completed');
 
 CREATE TABLE enrollments(
@@ -90,7 +79,6 @@ CREATE TABLE enrollments(
   updated_at TIMESTAMP NOT NULL
 );
 
--- Таблица для оплат
 CREATE TYPE payment_status AS ENUM('pending', 'paid', 'failed', 'refunded');
 
 CREATE TABLE payments(
@@ -103,7 +91,6 @@ CREATE TABLE payments(
   updated_at TIMESTAMP NOT NULL
 );
 
--- Таблица отслеживания прогресса прохождения программы
 CREATE TYPE program_completion_status AS ENUM('active', 'completed', 'pending', 'cancelled');
 
 CREATE TABLE program_completions(
@@ -117,7 +104,6 @@ CREATE TABLE program_completions(
   updated_at TIMESTAMP NOT NULL
 );
 
--- Таблица сертификатов
 CREATE TABLE certificates(
 	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	user_id BIGINT REFERENCES users(id) ON DELETE SET NULL,
@@ -128,7 +114,6 @@ CREATE TABLE certificates(
   updated_at TIMESTAMP NOT NULL
 );
 
--- Таблица для тестов
 CREATE TABLE quizzes(
 	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	lesson_id BIGINT REFERENCES lessons(id) ON DELETE SET NULL,
@@ -138,7 +123,6 @@ CREATE TABLE quizzes(
   updated_at TIMESTAMP NOT NULL
 );
 
--- Таблица для практических заданий
 CREATE TABLE exercises(
 	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	lesson_id BIGINT REFERENCES lessons(id) ON DELETE SET NULL,
@@ -148,7 +132,6 @@ CREATE TABLE exercises(
   updated_at TIMESTAMP NOT NULL
 );
 
--- Таблица обсуждений
 CREATE TABLE discussions(
 	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	user_id BIGINT REFERENCES users(id) ON DELETE SET NULL,
@@ -158,7 +141,6 @@ CREATE TABLE discussions(
   updated_at TIMESTAMP NOT NULL
 );
 
--- Таблица блога
 CREATE TYPE blog_status AS ENUM('created', 'in moderation', 'published', 'archived');
 
 CREATE TABLE blog(
