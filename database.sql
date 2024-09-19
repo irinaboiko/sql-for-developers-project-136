949a9ae40bb68ada0586873dfbbcb05712d0209a
irinaboiko <irinaboiko95@gmail.com>
2024-09-19 19:32:01 +0300

    fixing

--- database.sql
+++ database.sql
@@ -1,8 +1,8 @@
 -- Таблица уроков
 CREATE TABLE lessons(
-	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	course_id BIGINT REFERENCES courses(id) NOT NULL,
-	name VARCHAR(255) NOT NULL,
+	lesson_name VARCHAR(255) NOT NULL,
 	content TEXT NOT NULL,
 	video_link TEXT NOT NULL,
 	position INTEGER NOT NULL,
@@ -14,8 +14,8 @@ CREATE TABLE lessons(
 
 -- Таблица курсов
 CREATE TABLE courses(
-	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	name VARCHAR(255) NOT NULL,
+	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
+	course_name VARCHAR(255) NOT NULL,
 	description TEXT NOT NULL,
 	created_at TIMESTAMP NOT NULL,
 	updated_at TIMESTAMP NOT NULL,
@@ -24,8 +24,8 @@ CREATE TABLE courses(
 
 -- Таблица модулей
 CREATE TABLE modules(
-	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	name VARCHAR(255) NOT NULL,
+	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
+	module_name VARCHAR(255) NOT NULL,
 	description TEXT NOT NULL,
 	created_at TIMESTAMP NOT NULL,
 	updated_at TIMESTAMP NOT NULL
@@ -33,8 +33,8 @@ CREATE TABLE modules(
 
 -- Таблица программ обучения
 CREATE TABLE programs(
-	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-	name VARCHAR(255) NOT NULL,
+	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
+	program_name VARCHAR(255) NOT NULL,
 	cost INTEGER NOT NULL,
 	type VARCHAR(255) NOT NULL,
 	created_at TIMESTAMP NOT NULL,
@@ -59,7 +59,7 @@ CREATE TABLE program_modules(
 CREATE TYPE user_role AS ENUM('student', 'teacher', 'admin');
 
 CREATE TABLE users(
-	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	teaching_group_id BIGINT REFERENCES teaching_groups(id) ON DELETE SET NULL,
 	username VARCHAR(255) NOT NULL,
 	email VARCHAR(255) UNIQUE NOT NULL,
@@ -72,7 +72,7 @@ CREATE TABLE users(
 
 -- Таблица учебной группы пользователя
 CREATE TABLE teaching_groups(
-	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	slug VARCHAR(255) UNIQUE,
 	created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL
@@ -82,7 +82,7 @@ CREATE TABLE teaching_groups(
 CREATE TYPE enrollment_status AS ENUM('active', 'pending', 'cancelled', 'completed');
 
 CREATE TABLE enrollments(
-	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	user_id BIGINT REFERENCES users(id) ON DELETE SET NULL,
 	program_id BIGINT REFERENCES programs(id) ON DELETE SET NULL,
 	status enrollment_status NOT NULL,
@@ -94,7 +94,7 @@ CREATE TABLE enrollments(
 CREATE TYPE payment_status AS ENUM('pending', 'paid', 'failed', 'refunded');
 
 CREATE TABLE payments(
-	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	enrollment_id BIGINT REFERENCES enrollments(id) ON DELETE SET NULL,
 	amount NUMERIC NOT NULL,
 	status payment_status NOT NULL,
@@ -107,7 +107,7 @@ CREATE TABLE payments(
 CREATE TYPE program_completion_status AS ENUM('active', 'completed', 'pending', 'cancelled');
 
 CREATE TABLE program_completions(
-	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	user_id BIGINT REFERENCES users(id) ON DELETE SET NULL,
 	program_id BIGINT REFERENCES programs(id) ON DELETE SET NULL,
 	status program_completion_status NOT NULL,
@@ -119,7 +119,7 @@ CREATE TABLE program_completions(
 
 -- Таблица сертификатов
 CREATE TABLE certificates(
-	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	user_id BIGINT REFERENCES users(id) ON DELETE SET NULL,
   program_id BIGINT REFERENCES programs(id) ON DELETE SET NULL,
   certificate_url TEXT NOT NULL,
@@ -130,9 +130,9 @@ CREATE TABLE certificates(
 
 -- Таблица для тестов
 CREATE TABLE quizzes(
-	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	lesson_id BIGINT REFERENCES lessons(id) ON DELETE SET NULL,
-	name VARCHAR(255) NOT NULL,
+	quizze_name VARCHAR(255) NOT NULL,
 	content JSONB NOT NULL,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL
@@ -140,32 +140,32 @@ CREATE TABLE quizzes(
 
 -- Таблица для практических заданий
 CREATE TABLE exercises(
-	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
 	lesson_id BIGINT REFERENCES lessons(id) ON DELETE SET NULL,
-	name VARCHAR(255) NOT NULL,
+	exercise_name VARCHAR(255) NOT NULL,
 	url TEXT NOT NULL,
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL
 );
---
----- Таблица обсуждений
---CREATE TABLE discussions(
---	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
---	lesson_id BIGINT REFERENCES lessons(id) ON DELETE SET NULL,
---	content JSONB NOT NULL,
---  created_at TIMESTAMP NOT NULL,
---  updated_at TIMESTAMP NOT NULL
---);
---
----- Таблица блога
---CREATE TYPE blog_status AS ENUM('created', 'in moderation', 'published', 'archived');
---
---CREATE TABLE blog(
---	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
---	user_id BIGINT REFERENCES users(id) ON DELETE SET NULL,
---	title VARCHAR(255) NOT NULL,
---	content TEXT  NOT NULL,
---	status blog_status NOT NULL,
---  created_at TIMESTAMP NOT NULL,
---  updated_at TIMESTAMP NOT NULL
---);
+
+-- Таблица обсуждений
+CREATE TABLE discussions(
+	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
+	lesson_id BIGINT REFERENCES lessons(id) ON DELETE SET NULL,
+	content JSONB NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL
+);
+
+-- Таблица блога
+CREATE TYPE blog_status AS ENUM('created', 'in moderation', 'published', 'archived');
+
+CREATE TABLE blog(
+	id BIGINT PRIMARY KEY UNIQUE NOT NULL,
+	user_id BIGINT REFERENCES users(id) ON DELETE SET NULL,
+	title VARCHAR(255) NOT NULL,
+	content TEXT  NOT NULL,
+	status blog_status NOT NULL,
+  created_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP NOT NULL
+);
