69c2f426b5dd8b6f013a413889fa17c2d4a940ff
irinaboiko <irinaboiko95@gmail.com>
2024-09-20 13:02:59 +0300

    remove nulls

--- database.sql
+++ database.sql
@@ -9,7 +9,7 @@ CREATE TABLE courses(
 	name VARCHAR(255) NOT NULL,
 	description TEXT NOT NULL,
 	created_at TIMESTAMP NOT NULL,
-	updated_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP,
 	deleted_at TIMESTAMP
 );
 
@@ -18,10 +18,10 @@ CREATE TABLE lessons(
 	course_id integer REFERENCES courses(id),
 	name VARCHAR(255) NOT NULL,
 	content TEXT NOT NULL,
-	video_url TEXT NOT NULL,
+	video_url TEXT,
 	position INTEGER,
 	created_at TIMESTAMP NOT NULL,
-	updated_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP,
 	deleted_at TIMESTAMP
 );
 
@@ -30,7 +30,7 @@ CREATE TABLE modules(
 	name VARCHAR(255) NOT NULL,
 	description TEXT NOT NULL,
 	created_at TIMESTAMP NOT NULL,
-	updated_at TIMESTAMP NOT NULL,
+	updated_at TIMESTAMP,
 	deleted_at TIMESTAMP
 );
 
@@ -40,7 +40,7 @@ CREATE TABLE programs(
 	price INTEGER NOT NULL,
 	program_type VARCHAR(255) NOT NULL,
 	created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
+  updated_at TIMESTAMP
 );
 
 CREATE TABLE course_modules(
@@ -57,7 +57,7 @@ CREATE TABLE teaching_groups(
 	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	slug VARCHAR(255) UNIQUE,
 	created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
+  updated_at TIMESTAMP
 );
 
 CREATE TABLE users(
@@ -68,7 +68,7 @@ CREATE TABLE users(
 	password_hash TEXT NOT NULL,
 	role user_role NOT NULL,
 	created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL,
+  updated_at TIMESTAMP,
   deleted_at TIMESTAMP
 );
 
@@ -78,7 +78,7 @@ CREATE TABLE enrollments(
 	program_id BIGINT REFERENCES programs(id),
 	status enrollment_status NOT NULL,
 	created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
+  updated_at TIMESTAMP
 );
 
 CREATE TABLE payments(
@@ -86,9 +86,9 @@ CREATE TABLE payments(
 	enrollment_id BIGINT REFERENCES enrollments(id),
 	amount NUMERIC NOT NULL,
 	status payment_status NOT NULL,
-	paid_at TIMESTAMP NOT NULL,
+	paid_at TIMESTAMP,
 	created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
+  updated_at TIMESTAMP
 );
 
 CREATE TABLE program_completions(
@@ -99,17 +99,17 @@ CREATE TABLE program_completions(
 	started_at TIMESTAMP NOT NULL,
 	completed_at TIMESTAMP,
   created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
+  updated_at TIMESTAMP
 );
 
 CREATE TABLE certificates(
 	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	user_id BIGINT REFERENCES users(id),
   program_id BIGINT REFERENCES programs(id),
-  url TEXT NOT NULL,
-  issued_at TIMESTAMP NOT NULL,
+  url TEXT,
+  issued_at TIMESTAMP,
   created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
+  updated_at TIMESTAMP
 );
 
 CREATE TABLE quizzes(
@@ -118,16 +118,16 @@ CREATE TABLE quizzes(
 	name VARCHAR(255) NOT NULL,
 	content JSONB NOT NULL,
   created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
+  updated_at TIMESTAMP
 );
 
 CREATE TABLE exercises(
 	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	lesson_id BIGINT REFERENCES lessons(id),
 	name VARCHAR(255) NOT NULL,
-	url TEXT NOT NULL,
+	url TEXT,
   created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
+  updated_at TIMESTAMP
 );
 
 CREATE TABLE discussions(
@@ -136,15 +136,15 @@ CREATE TABLE discussions(
 	lesson_id BIGINT REFERENCES lessons(id),
 	text JSONB NOT NULL,
   created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
+  updated_at TIMESTAMP
 );
 
 CREATE TABLE blogs(
 	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	user_id BIGINT REFERENCES users(id),
 	name VARCHAR(255) NOT NULL,
-	content TEXT  NOT NULL,
+	content TEXT NOT NULL,
 	status blog_status NOT NULL,
   created_at TIMESTAMP NOT NULL,
-  updated_at TIMESTAMP NOT NULL
+  updated_at TIMESTAMP
 );
